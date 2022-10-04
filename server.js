fb1983def6c154b23a989626c1ffacb47ecda810
Phelipe Matias <phelipems91@gmail.com>
2022-10-03 20:00:55 -0400

    Initial page and folder structure done

---
+++ server.js
@@ -0,0 +1,13 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+var express = require('./config/express');
+var app = express();
+
+var HOST = 'localhost';
+var PORT = 3000;
+
+app.listen(PORT);
+
+console.log(`Server running at http://${HOST}:${PORT}/`);
+
+module.exports = app;
